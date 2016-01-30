c8ccc93e60403c9c1150a09505fe9c8e66064d01
Justin Obney <justinobney@gmail.com>
2016-01-30 09:56:24 -0600

    initial cleanup from asp.net web api template

---
+++ WebAPI_Learning_1/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebAPI_Learning_1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Redirect("/swagger");
+        }
+    }
+}
---
+++ WebAPI_Learning_1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPI_Learning_1.WebApiApplication" Language="C#" %>
